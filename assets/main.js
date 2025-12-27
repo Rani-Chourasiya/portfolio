b1b85e15d1fff6fcfb34218cdb7290bb6509e51a
Rani chourasiya <ranichourasiya98@gmail.com>
2025-12-27 17:03:14 +0530

    first commit

---
+++ assets/main.js
@@ -0,0 +1,38 @@
+ // navbar sticky
+ window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".main-navbar");
+
+    if (window.scrollY > 80) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  });
+  // tab js
+  const buttons = document.querySelectorAll('.filter-btn');
+  const projects = document.querySelectorAll('.project');
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+      buttons.forEach(b => b.classList.remove('active'));
+      btn.classList.add('active');
+
+      const filter = btn.dataset.filter;
+
+      projects.forEach(project => {
+        project.classList.remove('hide');
+
+        if (filter !== 'all' && !project.classList.contains(filter)) {
+          project.classList.add('hide');
+        }
+      });
+    });
+  });
+  // pdf js
+  function downloadCV() {
+    const pdfUrl = "assets/Resume Rani Chourasiya 2026.pdf";
+
+    // Open PDF only in a new tab
+    window.open(pdfUrl, "_blank", "noopener,noreferrer");
+  }
